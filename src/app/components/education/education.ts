e99a8a613d7b7ecfea623c72beba8f86bb552c51
Idriss <44480885+m-idriss@users.noreply.github.com>
2025-09-28 20:19:19 +0200

    Refactor education, experience, hobbies, and tech-stack sections to fetch data from NotionService; update components to standalone; add techStacks support to NotionService

--- src/app/components/education/education.ts
+++ src/app/components/education/education.ts
@@ -1,21 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { LinkItem } from '../../models/link-item.model';
-import { DataService } from '../../services/data.service';
+import { NotionService } from '../../services/notion.service';
 
 @Component({
   selector: 'app-education',
-  imports: [],
+  standalone: true,
   templateUrl: './education.html',
   styleUrl: './education.scss'
 })
 export class Education implements OnInit {
   education: LinkItem[] = [];
 
-  constructor(private readonly dataService: DataService) {}
+  constructor(private readonly notionService: NotionService) {}
 
   ngOnInit() {
-   this.dataService.getEducation().subscribe((data: LinkItem[]) => {
-     this.education = data;
+    this.notionService.fetchAll().subscribe(() => {
+      this.education = this.notionService.getEducations();
    });
   }
 }
--- src/app/components/tech-stack/tech-stack.ts
+++ src/app/components/tech-stack/tech-stack.ts
@@ -1,21 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { LinkItem } from '../../models/link-item.model';
-import { DataService } from '../../services/data.service';
+import { NotionService } from '../../services/notion.service';
 
 @Component({
   selector: 'app-tech-stack',
-  imports: [],
+  standalone: true,
   templateUrl: './tech-stack.html',
   styleUrl: './tech-stack.scss'
 })
 export class TechStack implements OnInit {
   techStack: LinkItem[] = [];
 
-  constructor(private readonly dataService: DataService) {}
+  constructor(private readonly notionService: NotionService) {}
 
   ngOnInit() {
-    this.dataService.getTechStacks().subscribe((data: LinkItem[]) => {
-      this.techStack = data;
+    this.notionService.fetchAll().subscribe(() => {
+      this.techStack = this.notionService.getTechStacks();
     });
   }
 }
